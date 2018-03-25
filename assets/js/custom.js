629bf2028930df18d24da3281271413103eed11e
mohit493 <mohityadav0493@gmail.com>
2018-03-25 20:34:37 +0200

    captcha added with alterations

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,17 +1,3 @@
-document.getElementById("ContactSubmitButton").disabled = true;
-
-function enableBtn() {
-    document.getElementById("ContactSubmitButton").disabled = false;
-}
-
 /*===================================================================================*/
 /*	CUSTOM JS/JQUERY SCRIPTS
 /*===================================================================================*/
-
-// Insert your own scripts in here!
-
-/*===================================================================================*/
-/*	CUSTOM JS/JQUERY SCRIPTS
-/*===================================================================================*/
-
-// Insert your own scripts in here!
