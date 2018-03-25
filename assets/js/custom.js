b98983f00f24d70b1694865ca1ec57647e1f0dd2
mohit493 <mohityadav0493@gmail.com>
2018-03-25 19:57:25 +0200

    Adding Captcha

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,4 +1,14 @@
+document.getElementById("ContactSubmitButton").disabled = true;
 
+function enableBtn() {
+    document.getElementById("ContactSubmitButton").disabled = false;
+}
+
+/*===================================================================================*/
+/*	CUSTOM JS/JQUERY SCRIPTS
+/*===================================================================================*/
+
+// Insert your own scripts in here!
 
 /*===================================================================================*/
 /*	CUSTOM JS/JQUERY SCRIPTS
